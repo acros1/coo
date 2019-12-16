c8c0372536a49fa17d2f400c37dc2802fe32745c
Alex <Alex@Alex-Dell>
2019-12-16 15:42:25 +0100

    Replacing maven project

---
+++ ChatSystemProject/src/chatsystemproject/UDPListener.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+import java.net.*;
+
+public class UDPListener implements Runnable {
+
+	private ListenerThread listenerThread = null;
+	private ClientThread clientThread = null; 
+
+	public UDPListener(ListenerThread listenerThread, ClientThread clientThread) {
+		this.listenerThread = listenerThread;
+		this.clientThread = clientThread;
+	}
+
+	public void run() {
+		try {
+			DatagramSocket dgramSocket = new DatagramSocket(4000);
+			byte[] buffer = new byte[256];
+			DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+			while(true) {
+				// New user is connected, datagram reception
+				dgramSocket.receive(inPacket);
+				String clientName = new String(inPacket.getData(), 0, inPacket.getLength());
+				System.out.println("New broadcast alert receive, new user : " + clientName);
+				InetAddress clientAddr = inPacket.getAddress();
+				// int clientPort = inPacket.getPort();
+				if (isUserRegistered(clientName) == false) {
+					sendUser(clientName, clientAddr);
+					System.out.println(clientName + " is not in the list yet," + 
+									" answering with : " + clientThread.getMainUserName());
+					// Answering the new connection alert
+					String response = clientThread.getMainUserName();
+
+					DatagramPacket outPacket = new DatagramPacket(response.getBytes(), 
+												response.length(), clientAddr, 4000);
+					dgramSocket.send(outPacket);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  	
+
+	}
+
+	public void sendUser(String name, InetAddress addr) {
+		listenerThread.addNewConnectedUser(name, addr);
+	}
+
+	public boolean isUserRegistered(String name) {
+		return listenerThread.isUserRegistered(name);
+	}
+
+}
+
