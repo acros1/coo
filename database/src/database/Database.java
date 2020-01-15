461a9320c355d0c70adba86d4b2838d81b0b9a02
Alex <Alex@Alex-Dell>
2020-01-15 10:11:39 +0100

    database implementation finished

--- database/src/database/Database.java
+++ database/src/database/Database.java
@@ -10,22 +10,33 @@ package database;
  * @author Alex
  */
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database {
-
+    
+    private static String RelativePath = "..\\..\\myDataBase.db";
+    private static String pathToAdd = "\\coo\\database\\myDataBase.db";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Connect connetion = new Connect("..\\..\\myDataBase.db");
+        File myDataBase = new File(RelativePath);
+        Connect connetion = null;
+        try {
+            connetion = new Connect(myDataBase.getParentFile().getCanonicalPath() + pathToAdd);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         connetion.connect();
         
         ResultSet resultSet = connetion.query("SELECT * FROM Users");
         try {
             while (resultSet.next()) {
-                System.out.println("Titre : " + resultSet.getString("Title"));
+                System.out.println("Login : " + resultSet.getString("login"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
