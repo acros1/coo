f1f08d5259eb8d3c1ffb6ab503c4d4401f180de3
Maelned <44137206+Maelned@users.noreply.github.com>
2020-02-01 15:55:30 +0100

    up

--- ChatSystemProject2/src/chatsystemproject/Session.java
+++ ChatSystemProject2/src/chatsystemproject/Session.java
@@ -33,7 +33,6 @@ public class Session {
         this.sW = new SessionWindow(user2, this.clientThread,this.clientThread.getDB());
         this.ServerThread = this.clientThread.getMainSystem().getServer(user2,this);       
         this.ServerThread.setSession(this);
-        this.sW.setServerThread(ServerThread);
         this.sW.setLocationRelativeTo(null);
         this.sW.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         this.sW.setVisible(true);
@@ -45,7 +44,6 @@ public class Session {
         this.user2 = user2;
         this.clientThread = clientThread;
         this.sW = new SessionWindow(user2, this.clientThread,this.clientThread.getDB());
-        this.sW.setServerThread(ServerThread);
         this.sW.setLocationRelativeTo(null);
         this.sW.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         this.sW.setVisible(true);
@@ -77,6 +75,7 @@ public class Session {
     }
     
     public void setServerThread(ServerThread st){
+        System.out.println("setting st and session");
         this.ServerThread = st;
         st.setSession(this);
     }
